8e8b34afda645f4d498a5b3009616b189fddc74c
Braintelligence <waldemarhamm@googlemail.com>
2016-03-26 00:46:00 +0100

    Last push today.

--- flarum/views.py
+++ flarum/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+
+    forum_title = "Forentitel"
+    forum_url = "http://forum.django.local/"
+
+
+    context = {
+        "title": forum_title,
+        "forum_url": forum_url
+    }
+
+    return render(request, "index.html", context)
+
