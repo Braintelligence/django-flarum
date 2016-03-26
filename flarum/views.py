e9fea61bc1a6a965f44f78fe20c0f4355b0df1c9
Braintelligence <waldemarhamm@googlemail.com>
2016-03-26 03:18:42 +0100

    Last push today (really this time). Changed some view logic, but still doesn't work for me.

--- flarum/views.py
+++ flarum/views.py
@@ -1,14 +1,26 @@
 from django.shortcuts import render
+import requests
+
+import json
+from django.conf import settings
 
 def index(request):
 
     forum_title = "Forentitel"
     forum_url = "http://forum.django.local/"
 
+    url = "http://forum.django.local/api/forum"
+    headers = {'content-type': 'application/vnd.api+json'}
+
+    session = requests.Session()
+    session.trust_env = False
+
+    response = session.get(url=url, headers=headers)
 
     context = {
         "title": forum_title,
-        "forum_url": forum_url
+        "forum_url": forum_url,
+        "api_result": response.text
     }
 
     return render(request, "index.html", context)
